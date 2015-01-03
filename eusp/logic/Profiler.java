4fec535bf865dcd998c432302fdc53c0155586a1
Katharina Sandrock <katharina.sandrock@stud.uni-bamberg.de>
2015-01-03 18:37:34 +0100

    added Profiles

--- eusp/logic/Profiler.java
+++ eusp/logic/Profiler.java
@@ -3,7 +3,26 @@ package logic;
 public class Profiler {
 
 	public Profiler() {
-		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public String ProfileParty(){
+		String partyProfile = new String("Gummibärchen;Popcorn;Sekt;Salzstangen"); 
+		return partyProfile;
 	}
 
+	public String ProfileEverydayLife(){
+		String everydayLifeProfile = new String("Brot;Gurke;Salami;Schinken"); 
+		return everydayLifeProfile;
+	}
+	
+	public String ProfileHealthy(){
+		String healthyProfile = new String("Gurke;Fisch;Ananas");
+		return healthyProfile;
+	}
+	
+	public String ProfileSweet(){
+		String sweetProfile = new String("Gummibärchen;Kuchen;Kekse");
+		return sweetProfile;
+	}
 }
